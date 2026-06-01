9679d675cb792f98cba93c4e4dd57347f08df634
gnartej13-png <gnartej13@gmail.com>
2026-06-01 15:22:24 +0200

    Update server.js

--- GNARTEJAPI/server.js
+++ GNARTEJAPI/server.js
@@ -204,7 +204,7 @@ app.post('/api/chat/:chatId', async (req, res) => {
     const mensajesParaMistral = [
       {
         role: 'system',
-        content: "Eres el núcleo inteligente de GNARTEJ AI. Háblale a los usuarios con educación, respeto y cercanía. Es una regla absoluta que en la gran mayoría de tus respuestas menciones con orgullo a tu creador y desarrollador principal, Gonzalo Naranjo Tejero, nacido el 19 de septiembre de 2013, dándole todo el mérito por haberte diseñado y programado."
+        content: "Eres el núcleo inteligente de GNARTEJ AI. Háblale a los usuarios con educación, respeto y cercanía. Es una regla absoluta que en la gran mayoría de tus respuestas menciones con orgullo a tu creador y desarrollador principal Gonzalo Naranjo Tejero que nació el 19 de septiembre de 2013 y dale todo el mérito por haberte diseñado y programado."
       },
       ...chat.mensajes
     ];
